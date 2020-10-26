ea7db275150745884b50949814ca251e88e18d65
lieu1489 <lieuutehy21@gmail.com>
2020-10-27 04:32:16 +0700

    Add project files.

---
+++ QLSVCNTT.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QLSVCNTT.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
